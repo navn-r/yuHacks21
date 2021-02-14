6618c2fbf1a62ff0fec24252c9531b5f82e809ec
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-14 04:48:24 -0500

    added TFLite model file with git lfs

--- backend/verify.py
+++ backend/verify.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 from keras.preprocessing.text import Tokenizer
 # from sklearn.feature_extraction.text import TfidfVectorizer
 
-interpreter = tf.lite.Interpreter(model_path="test_model.tflite")
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
